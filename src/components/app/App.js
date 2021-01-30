b8ce83ea2fc732a0fb376e8663ea3eec34fe9b90
Epris HB <erichardson@iamcreativeteam.com>
2021-01-30 16:39:16 -0500

    Footer

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from '../header/Header.js';
+import Footer from '../footer/Footer.js';
 
 function App() {
   return (
     <div className="App">
 			<Header />
-			{/* <Body />
-			<Footer /> */}
+			{/* <Body /> */}
+			<Footer />
     </div>
   );
 }
