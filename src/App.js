700a70df84a69b5935acacb181a378b658242a19
Epris HB <erichardson@iamcreativeteam.com>
2021-03-02 19:49:32 -0500

    About Layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import { Route, Switch } from 'react-router-dom'
+import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import Header from './components/Header'
 import Home from './components/Home'
 import styled from 'styled-components'
 
+const theme = createMuiTheme({
+   typography: {
+    "fontFamily": `"Lato", "Helvetica", "Arial", sans-serif`,
+    "fontSize": 16,
+    "fontWeightLight": 300,
+    "fontWeightRegular": 400,
+    "fontWeightMedium": 500
+   }
+});
+
 function App() {
 	return (
-		<Wrapper>
-			<Header />
-			<Switch>
-				<Route exact path="/SevenTimesDesign" component={Home}></Route>
-			</Switch>
-			{/* {window.location.pathname !== "/Project" && <Footer />} */}
-    </Wrapper>
+		<ThemeProvider theme={theme}>
+			<Wrapper>
+				<Header />
+				<Switch>
+					<Route exact path="/SevenTimesDesign" component={Home}></Route>
+				</Switch>
+				{/* {window.location.pathname !== "/Project" && <Footer />} */}
+			</Wrapper>
+		</ThemeProvider>
   );
 }
 
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,8 +1,13 @@
 import React from 'react'
-import { Container, Typography } from '@material-ui/core'
+import { Container, Grid, Typography } from '@material-ui/core'
 import { styled as MaterialStyled } from '@material-ui/core/styles'
+import FaceIcon from '@material-ui/icons/Face'
+import FireplaceIcon from '@material-ui/icons/Fireplace'
+import LocalDiningIcon from '@material-ui/icons/LocalDining'
+import SportsBasketballIcon from '@material-ui/icons/SportsBasketball'
 import styled from 'styled-components'
-import image from '../assets/profile_web.png'
+import heroImage from '../assets/profile_web.png'
+import profileImage from '../assets/EprisHB.jpg'
 
 function Home() {
 	return (
@@ -20,8 +25,28 @@ function Home() {
 					</HeroContent>
 				</Container>
 			</Hero>
-			<Section>
-				<Container maxWidth="md">About</Container>
+			<Section className="about">
+				<Container maxWidth="md" justify="center" alignItems="center">
+					<Grid container spacing={5}>
+						<Grid item xs={12} sm={5}>
+							<img src={profileImage} alt="Epris Harris-Burnett" width="300" height="auto" />
+						</Grid>
+						<Grid item xs={12} sm={7} className="aboutme">
+							<Grid>
+								<hr />
+								<br />
+								<AboutMe>About Me</AboutMe>
+								<Intro>Hello, I'm &Eacute;pris Harris-Burnett</Intro>
+								<br/>
+								<p>I'm web developer and graphic designer in Charlotte, NC.</p>
+								<br/>
+								<p>I fell in love with technology at a young age. Around the age of eight I discovered I was a natural at drawing. During the MySpace years, I decided to merge the two loves together by customizing pages for friends. Ultimately, graphic design was the path I walked before finding my way back into developing. Now, I'm back to enoying the best of both worlds; where creativity and logic coincide.</p>
+								<br />
+								<hr />
+							</Grid>
+						</Grid>
+					</Grid>
+				</Container>
 			</Section>
 			<Section>
 				<Container maxWidth="md">Experience</Container>
@@ -45,11 +70,22 @@ const Wrapper = styled.div `
 const Section = styled.section `
 	height: 100vh;
 	color: #ccdbe5;
+
+	&.about {
+		display: flex;
+		align-items: center;
+	}
+
+	.aboutme {
+		display: grid;
+		place-items: center;
+		background-color: #BF4953;
+	}
 `
 const Hero = styled.section `
 	height: 100vh;
 	padding-top: 64px;
-	background-image: url(${image});
+	background-image: url(${heroImage});
 	background-size: cover;
 `
 
@@ -97,4 +133,15 @@ const HeroButton = styled.button `
 	margin: 16px 16px 16px;
 	padding: 16px;
 	border: 2px solid #BF4953;
-`	
+`
+
+const AboutMe = MaterialStyled(Typography) ({
+	fontSize: '24px',
+	fontWeight: 300,
+	color: '#202945',
+})
+
+const Intro = MaterialStyled(Typography) ({
+	fontSize: '30px',
+	fontWeight: 700,
+})
