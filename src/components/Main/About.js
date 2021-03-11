09f61ba745cc76a9a9e98ac1512ade98f804e629
Epris HB <erichardson@iamcreativeteam.com>
2021-03-11 13:15:20 -0500

    Typo In About

--- src/components/Main/About.js
+++ src/components/Main/About.js
@@ -12,7 +12,7 @@ const About = () => {
 				<Details>
 					<Intro>Hello, I'm &Eacute;pris Harris-Burnett</Intro>
 					<p className="intro">I'm web developer and graphic designer in Charlotte, NC.</p>
-					<p>I fell in love with technology at a young age. Around the age of eight I discovered I was a natural at drawing. During the MySpace years, I decided to merge the two talents together by customizing pages for friends. Ultimately, graphic design was the path I walked before finding my way back into developing. Now, I'm enoying the best of both worlds; where creativity and logic coincide.</p>
+					<p>I fell in love with technology at a young age. Around the age of eight I discovered I was a natural at drawing. During the MySpace years, I decided to merge the two talents together by customizing pages for friends. Ultimately, graphic design was the path I walked before finding my way back into developing. Now, I'm enjoying the best of both worlds; where creativity and logic coincide.</p>
 				</Details>
 			</Container>
 		</Section>
